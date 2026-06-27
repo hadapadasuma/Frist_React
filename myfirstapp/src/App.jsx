20014a3834c1d3bb44d530ba77dfc64805b341d0
Hadapada Suma <hsumasuma148@gmail.com>
2026-06-27 20:10:57 +0530

    reactcode

---
+++ myfirstapp/src/App.jsx
@@ -0,0 +1,12 @@
+//functional componets
+function App() {
+  return(
+    <div>
+      <h1>hello Everyone welcome to react</h1>
+      <h5>Bye, Thank you today</h5>
+    </div>
+    
+  );
+}
+
+export default App;
